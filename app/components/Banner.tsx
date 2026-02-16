47ca4b634f0845e51527e3d0e208d6046ef1e245
hades-codes <stephenadetoyi@gmail.com>
2026-02-16 17:20:41 +0100

    update 16/2/26

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <>
       <div className='absolute top-0 right-0 -translate-y-12 translate-x-12 blur-3xl opacity-20 pointer-events-none'>
-        <div className='aspect-square w-125 rounded-full bg-blue-400'/>
+        <div className='aspect-square w-125 rounded-full bg-blue-400'/> 
       </div>
       <div className='absolute bottom-0 left-0 translate-y-12 -translate-x-12 blur-3xl opacity-20 pointer-events-none'>
         <div className='aspect-square w-125 rounded-full bg-emerald-400'/>
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { motion, Variants } from 'framer-motion'
 
 
@@ -47,8 +47,8 @@ const navLogoVariants: Variants = {
     }
   }
 }
-
 const Nav = () => {
+const [isOpen, setIsOpen] = useState(false)
 
   return (
     <motion.div
@@ -61,14 +61,29 @@ const Nav = () => {
         <i className='bx bx-check-shield text-blue-600 text-[30px]'></i>
         <h3 className='text-[#1c398e] text-xl font-bold'>BILL BOT</h3>
       </motion.div>
-      <div className="flex items-center gap-3">
-        <ul className='gap-6 flex items-center font-medium text-base'>
-            <motion.li variants={navListVariants} className='hover:text-blue-600 cursor-pointer'>How it works</motion.li>
-            <motion.li variants={navListVariants} className='hover:text-blue-600 cursor-pointer'>Pricing</motion.li>
-            <motion.li variants={navListVariants} className='hover:text-blue-600 cursor-pointer'>Trust & Transparency</motion.li>
-        </ul>
-        <motion.button variants={navLogoVariants} type="button" className='text-white bg-blue-600 rounded-lg p-[5px] font-medium hover:bg-blue-700'>Upload Your Bill</motion.button>
-        </div> 
+      <div className="flex items-center gap-3 md:flex ">
+        <motion.div 
+        className='gap-6 flex items-center font-medium text-base hidden md:flex'>
+            <motion.a variants={navListVariants} className='hover:text-blue-600 cursor-pointer'>How it works</motion.a>
+            <motion.a variants={navListVariants} className='hover:text-blue-600 cursor-pointer'>Pricing</motion.a>
+            <motion.a variants={navListVariants} className='hover:text-blue-600 cursor-pointer'>Trust & Transparency</motion.a>
+        </motion.div>
+        <motion.button variants={navLogoVariants} type="button" className='text-white bg-blue-600 rounded-lg p-[5px] font-medium hover:bg-blue-700 hidden md:flex active:scale-[0.95]'>Upload Your Bill</motion.button>
+        </div>
+
+        <button onClick={() => setIsOpen(!isOpen)} className=' focus:outline-none md:hidden'>   
+            <i className={isOpen ? 'bx bx-x text-[30px]' : 'bx bx-menu text-[30px]'}></i>
+        </button> 
+
+        {isOpen && (
+          <motion.div className='absolute shadow-md top-16 w-full z-50 left-0 md:hidden p-[4px] bg-white '>
+            <a href="" className="block hover:text-blue-600">How it works</a>
+            <a href="" className="block hover:text-blue-600">Pricing</a>
+            <a href="" className="block hover:text-blue-600">Trust & Transparency</a>
+          <button type="button" className='text-white bg-blue-600 rounded-lg p-[5px] font-medium hover:bg-blue-700 active:scale-[0.95] w-full'>Upload Your Bill</button>
+          </motion.div>
+        )}
+         
       
     </motion.div>
   )
